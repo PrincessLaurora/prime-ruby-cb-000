bd28c9bf91e5e76b093f74c00c01fb81042fb548
Laura Felce <crepu83@yahoo.it>
2018-11-21 15:36:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
 def prime?(integer)
-integer.collect.to_a {|n| integer.include?[(new_integer % n) == 0]}
+new_integer = integer.to_a  
+new_integer.collect {|n| new_integer.include?[(new_integer % n) == 0]}
 end
